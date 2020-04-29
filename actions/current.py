9d4c70423e1c2a4980560a2a30aacf7d07fad2c0
Noah Houpt <nah70@case.edu>
2020-04-28 21:10:51 -0400

    it prints your csv

--- actions/current.py
+++ actions/current.py
@@ -3,11 +3,9 @@ import os.path
 
 def read_buys():
   buys_csv_path = os.path.dirname(__file__) + '/../buys.csv'
-
   buys = pd.read_csv(buys_csv_path)
 
-  for row in buys:
-    print(row)
+  print(buys)
 
 def print_portfolio_value():
   read_buys()
